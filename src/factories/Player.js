29800ac47921b5166cc773c29883a16d04e10dcb
Matt Haffner <mrhaffner13@gmail.com>
2020-11-04 11:45:18 -0600

    added some logic and test for Player array, still ironing out getting back non-duplicate coordinates

--- src/factories/Player.js
+++ src/factories/Player.js
@@ -1,12 +1,48 @@
 const Player = (name, type) => {
     //players can take turns playing the game by attacking the enemy Gameboard.
-    const attack = () => {
+    const getRandomCoords = (arr) => {
+        let getInt = () => Math.floor(Math.random() * Math.floor(2));
+        arr.push(getInt());
+        arr.push(getInt());
+        return arr;
+    }
+
+    const checkMisses = (arr, board) => {
+        for (let miss of board.misses) {   
+            if (arr[0] === miss[0] && arr[1] === miss[1]) {
+                arr = []
+                getRandomCoords(arr);
+                checkCoords(arr, board);
+                return arr;
+            } 
+        }
+    }
+
+    const checkShips = (arr, board) => {
+        for (let shipObj of board.ships) {
+            for (let ship of shipObj.hits) {  
+                if (arr[0] === ship[0] && arr[1] === ship[1]) {
+                    arr = []
+                    getRandomCoords(arr);
+                    checkCoords(arr, board);
+                    return arr;
+                }
+            }
+        }
+    }
+
+    const checkCoords = (arr, board) => {
+        //Ship.misses
+        checkMisses(arr, board);
+        //Ship.Ships.hits
+        checkShips(arr, board);
+    }
+
+    const attack = (board) => {
         if (type === 'CPU') {
-            //math.Random integers for two coordinates, within a specific range
-            let getInt = () => Math.floor(Math.random() * Math.floor(10))
-            let arr = []
-            arr.push(getInt())
-            arr.push(getInt())
+            let arr = [];
+            getRandomCoords(arr);
+            checkCoords(arr, board);
             return arr;
         }
                 //if those coordinates are in Ship.misses OR loops ships array in Ship.Ships.hits
@@ -15,7 +51,7 @@ const Player = (name, type) => {
             //select coords to enter
         //coords get fed to proper gameboard (board 1 = human, board 2 = CPU)
 
-    }
+    };
     return { name, attack, type };
 };
 
--- src/factories/Player.test.js
+++ src/factories/Player.test.js
@@ -2,13 +2,56 @@ import Player from './Player'
 
 test('tests to see if Player.attack() return an integer from 0-10', () => {
     const Player2 = Player('CPU', 'CPU')
-    expect(Player2.attack()[0]).toBeGreaterThanOrEqual(0);
-    expect(Player2.attack()[0]).toBeGreaterThanOrEqual(0);
-    expect(Player2.attack()[1]).toBeLessThanOrEqual(10);
-    expect(Player2.attack()[1]).toBeLessThanOrEqual(10);
+    const board = {
+        misses: [],
+        ships: [
+            {
+                hits: []
+            }
+        ],
+    }
+    expect(Player2.attack(board)[0]).toBeGreaterThanOrEqual(0);
+    expect(Player2.attack(board)[0]).toBeGreaterThanOrEqual(0);
+    expect(Player2.attack(board)[1]).toBeLessThanOrEqual(10);
+    expect(Player2.attack(board)[1]).toBeLessThanOrEqual(10);
 });
 
 test('tests to see if Player.attack() will not return coordinates if type does not equal CPU', () => {
+    const board = {
+        misses: [],
+        ships: [
+            {
+                hits: []
+            }
+        ],
+    }
     const Player2 = Player('CPU', 'Player')
-    expect(Player2.attack()).toBe(undefined);
+    expect(Player2.attack(board)).toBe(undefined);
+});
+
+
+test('tests to see if Player.attack() will work with coords in hits array', () => {
+    const board = {
+        misses: [],
+        ships: [
+            {
+                hits: [[0, 0], [0, 1], [1, 0]]
+            }
+        ],
+    }
+    const Player2 = Player('CPU', 'CPU')
+    expect(Player2.attack(board)).toStrictEqual([1, 1]);
+});
+
+test('tests to see if Player.attack() will work with coords in misses array', () => {
+    const board = {
+        misses: [[0, 0], [0, 1], [1, 0]],
+        ships: [
+            {
+                hits: []
+            }
+        ],
+    }
+    const Player2 = Player('CPU', 'CPU')
+    expect(Player2.attack(board)).toStrictEqual([1, 1]);
 });
